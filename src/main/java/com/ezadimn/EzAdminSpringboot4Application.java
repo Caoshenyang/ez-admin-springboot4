ab714ac3731701c3e4ef20c5e4bd7256c54d40d5
阳 <oncwnuFWU7J5-gS82IOk7296vO6I@git.weixin.qq.com>
2025-12-03 14:48:43 +0800

    feat: 项目初始化

--- src/main/java/com/ezadimn/EzAdminSpringboot4Application.java
+++ src/main/java/com/ezadimn/EzAdminSpringboot4Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EzAdminSpringboot4Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EzAdminSpringboot4Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EzAdminSpringboot4Application.class, args);
+    }
 
 }
