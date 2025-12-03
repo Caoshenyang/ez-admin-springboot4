f61cc4ec79a9bfc2286fe95bbc8547a31f580d71
阳 <oncwnuFWU7J5-gS82IOk7296vO6I@git.weixin.qq.com>
2025-12-03 14:36:02 +0800

    first commit

---
+++ src/main/java/com/ezadimn/EzAdminSpringboot4Application.java
@@ -0,0 +1,13 @@
+package com.ezadimn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzAdminSpringboot4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzAdminSpringboot4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezadimn/EzAdminSpringboot4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezadimn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzAdminSpringboot4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
